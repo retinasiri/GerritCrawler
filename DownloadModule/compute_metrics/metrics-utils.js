49f1e41516f9df13c36ee8078e2015a918e8792d
Jefferson Olongo <aurelien-jefferson.olongo-onana-noah.1@ens.etsmtl.ca>
2021-05-08 13:22:04 -0400

    correct minor bug

--- DownloadModule/compute_metrics/metrics-utils.js
+++ DownloadModule/compute_metrics/metrics-utils.js
@@ -116,8 +116,8 @@ function startComputeMetrics(projectName, metricsType, collectMetrics) {
             let tasks = []
             for (let i = 0; i < NUM_CONCURRENCY; i++) {
                 //let skip = NUM_OF_CHANGES_LIMIT * i;
-                skip = 17000;
-                NUM_OF_CHANGES_LIMIT = 3000;
+                skip = 20000;
+                NUM_OF_CHANGES_LIMIT = 5000;
                 console.log("Processing metrics from " + skip + " to " + (skip + NUM_OF_CHANGES_LIMIT));
                 let t = getChanges(skip, NUM_OF_CHANGES_LIMIT,
                     Project, MetricsJson, progressBar, collectMetrics);
